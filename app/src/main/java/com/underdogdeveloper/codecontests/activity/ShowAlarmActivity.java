52a5e199ea9f2315613e9b7c9ff1fac01ea1149c
Nipun Kumar <nipunvirat0@gmail.com>
2021-06-28 10:55:02 +0530

    Added alarm activity

---
+++ app/src/main/java/com/underdogdeveloper/codecontests/activity/ShowAlarmActivity.java
@@ -0,0 +1,16 @@
+package com.underdogdeveloper.codecontests.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.underdogdeveloper.codecontests.R;
+
+public class ShowAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_alarm);
+    }
+}
